570dadf38bbb48a90cc9e1ad3bea3ff71b74e992
Benjamin Wall <bwall4@gmu.edu>
2024-12-20 11:41:35 -0500

    memset call does not work in mem.c for some reason

--- kern/mem/mem.c
+++ kern/mem/mem.c
@@ -1,5 +1,7 @@
 #include <kern/mem/mem.h>
 
+#include <kern/lib.h>
+
 /* TODO: Implement real kernel allocator? */
 static char bump_heap[16384];
 static char *heap_next = bump_heap;
@@ -8,7 +10,8 @@ void *kzalloc(size_t size) {
     char *result = heap_next;
     /* Ensure we're always 16 byte aligned */
     heap_next += size + ((16 - size) % 16);
-    /* TODO Implement memset */
+    /* TODO Figure out why memset causes a crash */
+    //memset(result, 0, size);
     for(size_t i = 0; i < size; ++i) {
         result[i] = 0;
     }
