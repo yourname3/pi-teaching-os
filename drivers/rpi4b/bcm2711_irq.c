bf0fcd155433d77dbe010b38f7bbfb1047282b69
Benjamin Wall <bwall4@gmu.edu>
2024-12-21 15:23:45 -0500

    Try to use TIMER_CNTRL0 register; Likely need to switch to GIC-400

--- drivers/rpi4b/bcm2711_irq.c
+++ drivers/rpi4b/bcm2711_irq.c
@@ -6,6 +6,14 @@
 
 #define TIMER_CNTRL0 (*(volatile uint32_t*)(ARM_LOCAL + 0x40))
 
+/**
+ * TODO:
+ * It appears that the "legacy" interrupt controller is not going to just work,
+ * probably--I'm not entirely sure what QEMU does but that's what I'm thinking.
+ * 
+ * So we probably want to just use the GIC-400 instead.
+ */
+
 void
 bcm2711_irq_init() {
     
