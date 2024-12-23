a5bc28fb1a89cae6657f48ee25331d98e36eefda
Benjamin Wall <bwall4@gmu.edu>
2024-12-22 23:57:20 -0500

    More cleanup

--- drivers/aarch64/el1_physical_timer.c
+++ drivers/aarch64/el1_physical_timer.c
@@ -3,8 +3,6 @@
 #include <kern/lib.h>
 #include <kern/irq.h>
 
-static uint64_t ticks_per_wait;
-
 static inline void
 set_cntp_tval_el0(uint64_t val) {
     __asm__("msr cntp_tval_el0, %0" : : "r" (val));
@@ -39,12 +37,6 @@ el1_timer_setup() {
     /* Register an IRQ for the timer firing. */
     irq_register(IRQ_EL1_PHYSICAL_TIMER, el1_timer_irq, NULL);
 
-    uint64_t freq = get_cntfrq_el0();
-    uint64_t desired_ms_per_tick = 20;
-    /* milliseconds per timer fire */
-    ticks_per_wait = freq * desired_ms_per_tick / 1000;
-    printk("el1 timer: freq = %d ticks = %d\n", freq, ticks_per_wait);
-
     set_cntp_ctl_el0(1 << 0);
-    set_cntp_tval_el0(freq);
+    set_cntp_tval_el0(get_cntfrq_el0());
 }
